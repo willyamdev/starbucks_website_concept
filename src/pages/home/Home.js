36b5b32d4e332a775d41888eb645a692a827001b
= <willyamvinicius1900@gmail.com>
2020-04-17 13:29:39 -0300

    added coffee home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,16 +3,21 @@ import './css/home.css';
 
 import image1 from './images/image1.png';
 import image2 from './images/image2.png';
-import image3 from './images/image3.png';
 
 export default function Home() {
   return (
     <div className="content">
       <div className="image-home-container">
         <img src={image1} alt=""></img>
+        <h3>$0 delivery fee for Starbucks orders on Uber Eats</h3>
+        <p>Get your favorite food and drinks delivered.</p>
+        <button>Order now</button>
       </div>
       <div className="image-home-container">
         <img src={image2} alt=""></img>
+        <h3>Brew and enjoy from the comfort of home</h3>
+        <p>Get your Starbucks favorites from your grocery provider.</p>
+        <button>Learn more</button>
       </div>
     </div>
   );
