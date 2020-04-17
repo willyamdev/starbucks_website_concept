573a67afb350e9c881e57e66708ebc9f7edf12ee
= <willyamvinicius1900@gmail.com>
2020-04-17 13:17:13 -0300

    added coffee home

--- src/pages/components/toolbar/Toolbar.js
+++ src/pages/components/toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/toolbar.css';
 import logo from './images/logo.png';
 import { Link } from 'react-router-dom';
@@ -7,16 +7,53 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function Toolbar() {
 
+  let [linkSelected, setSelectedLink] = useState(0);
+
+  let links = [
+    {
+      index: 1,
+      linkText: "COFFEES",
+      rote: "/coffees"
+    },
+    {
+      index: 2,
+      linkText: "COFFEEHOUSE",
+      rote: "/"
+    },
+    {
+      index: 3,
+      linkText: "SOCIAL IMPACT",
+      rote: "/"
+    },
+    {
+      index: 4,
+      linkText: "STARBUCKS REWARDS",
+      rote: "/"
+    },
+    {
+      index: 5,
+      linkText: "STORIES",
+      rote: "/"
+
+    }
+  ];
+
   return (
     <header>
       <nav>
         <img src={logo} alt="error"></img>
         <div className="toolbar-itens-container">
-          <Link to="/coffees" className="selected-link">COFFEES</Link>
-          <Link to="/">COFFEEHOUSE</Link>
-          <Link to="/">SOCIAL IMPACT</Link>
-          <Link to="/">STARBUCKS REWARDS</Link>
-          <Link to="/">STORIES</Link>
+
+          {links.map((value, index) => {
+
+            if (linkSelected === value.index) {
+              return <Link to={value.rote} className="selected-link" onClick={()=>setSelectedLink(value.index)}>{value.linkText}</Link>;
+            } else {
+              return <Link to={value.rote} onClick={()=>setSelectedLink(value.index)}>{value.linkText}</Link>
+            }
+
+          })}
+          
         </div>
         <div className="background-search">
           <FontAwesomeIcon icon={faSearch} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './css/home.css';
+
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
+import image3 from './images/image3.png';
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="content">
+      <div className="image-home-container">
+        <img src={image1} alt=""></img>
+      </div>
+      <div className="image-home-container">
+        <img src={image2} alt=""></img>
+      </div>
     </div>
   );
 }
