628f8beb49e2276996b891ba3ec4cab2fc7c1365
= <willyamvinicius1900@gmail.com>
2020-04-16 22:26:29 -0300

    added coffee page

--- src/pages/coffees/CoffeeContentCell.js
+++ src/pages/coffees/CoffeeContentCell.js
@@ -1,54 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/coffees.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
-
-import testeimg from './images/coffees-icons/blonde-caffee-americano.png';
 
 import coffeSizeSmall from './images/coffee-sizes/coffe-icon-small.png';
 import coffeSizeMedium from './images/coffee-sizes/coffe-icon-medium.png';
 import coffeSizeLarge from './images/coffee-sizes/coffe-icon-largel.png';
 
+import coffeSizeSmallEnabled from './images/coffee-sizes/coffe-icon-small-enabled.png';
+import coffeSizeMediumEnabled from './images/coffee-sizes/coffe-icon-medium-enabled.png';
+import coffeSizeLargeEnabled from './images/coffee-sizes/coffe-icon-largel-enabled.png';
+
 export default function CoffeeContentCell(props) {
+
+  let stars = [];
+  let coffeeSizes = [
+    {
+      index: 1,
+      coffeeSizeImage: coffeSizeSmall,
+      coffeeSizeEnableImage: coffeSizeSmallEnabled
+    },
+    {
+      index: 2,
+      coffeeSizeImage: coffeSizeMedium,
+      coffeeSizeEnableImage: coffeSizeMediumEnabled
+    },
+    {
+      index: 3,
+      coffeeSizeImage: coffeSizeLarge,
+      coffeeSizeEnableImage: coffeSizeLargeEnabled
+    },
+  ]
+
+  let [coffeePrice, setCoffeePrice] = useState(props.coffeeprice);
+  let [coffeeAmount, setCoffeeAmount] = useState(1);
+
+  let [coffeeSizeSelected, setCoffeeSize] = useState(1);
+
+  useEffect(() => {
+    setCoffeePrice(props.coffeeprice);
+  }, [props.coffeeprice]);
+
+  useEffect(() => {
+    setCoffeePrice(props.coffeeprice * coffeeAmount);
+  }, [coffeeAmount, props.coffeeprice]);
+
+  function changePrice(action) {
+    if (action) {
+      setCoffeeAmount(coffeeAmount + 1);
+    } else {
+      if (coffeeAmount === 1) return;
+      setCoffeeAmount(coffeeAmount - 1);
+    }
+  }
+
+  function cancelButton(){
+    setCoffeeAmount(1);
+    setCoffeeSize(1);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (i < props.coffeestars) {
+      stars.push(<FontAwesomeIcon icon={faStar} className="star star-selected" />);
+    } else {
+      stars.push(<FontAwesomeIcon icon={faStar} className="star" />);
+    }
+  }
+
   return (
     <div className="content">
       <div className="image-container">
-        <img src={testeimg} alt="" />
+        <img src={props.coffeeicon} alt="" />
       </div>
       <div className="content-container">
         <h2>{props.coffeeName}</h2>
         <p>{props.description}</p>
         <div class="stars-container">
-          <FontAwesomeIcon icon={faStar} className="star star-selected" />
-          <FontAwesomeIcon icon={faStar} className="star star-selected" />
-          <FontAwesomeIcon icon={faStar} className="star star-selected" />
-          <FontAwesomeIcon icon={faStar} className="star" />
-          <FontAwesomeIcon icon={faStar} className="star" />
+          {stars}
         </div>
         <div className="coffee-sizes-container">
-          <div className="coffe-size">
-            <img src={coffeSizeSmall} alt="" />
-          </div>
-          <div className="coffe-size">
-            <img src={coffeSizeMedium} alt="" />
-          </div>
-          <div className="coffe-size">
-            <img src={coffeSizeLarge} alt="" />
-          </div>
+
+          {coffeeSizes.map((value, index) => {
+            if (value.index === coffeeSizeSelected) {
+              return <div className="coffe-size coffe-size-selected" onClick={()=> setCoffeeSize(value.index)}>
+                <img src={value.coffeeSizeEnableImage} alt="" />
+              </div>
+            } else {
+              return <div className="coffe-size" onClick={()=> setCoffeeSize(value.index)}>
+                <img src={value.coffeeSizeImage} alt="" />
+              </div>
+            }
+          })}
         </div>
         <div className="price-and-amount">
-          <h2>5.54$</h2>
+          <h2>${coffeePrice.toFixed(2)}</h2>
           <div className="select-amount-container">
-            <FontAwesomeIcon icon={faAngleLeft} className="icon"/>
-            <h4>1</h4>
-            <FontAwesomeIcon icon={faAngleRight} className="icon"/>
+            <button onClick={() => changePrice(false)}> <FontAwesomeIcon icon={faAngleLeft} className="icon" /></button>
+            <h4>{coffeeAmount}</h4>
+            <button onClick={() => changePrice(true)} ><FontAwesomeIcon icon={faAngleRight} className="icon" /></button>
           </div>
         </div>
         <div className="buy-buttons-container">
           <button>Place Order</button>
-          <h4>Cancel</h4>
+          <h4 onClick={cancelButton}>Cancel</h4>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
--- src/pages/coffees/Coffees.js
+++ src/pages/coffees/Coffees.js
@@ -5,36 +5,83 @@ import CoffeeContentCell from './CoffeeContentCell';
 
 import blondeCaffeAmericano from './images/coffees-icons/blonde-caffee-americano.png';
 import caffeeMisto from './images/coffees-icons/caffee-misto.png';
+import darkRoastCoffee from './images/coffees-icons/dark-roast coffee.png';
+import cappuccino from './images/coffees-icons/cappuccino.png';
+import espressoconPanna from './images/coffees-icons/espresso-con-panna.png';
+import oatmilkHoneyLatte from './images/coffees-icons/oatmilk-honey-latte.png';
 
 export default function Coffees() {
 
   let coffees = [
     {
-      coffeeIcon:blondeCaffeAmericano,
+      coffeeIcon: blondeCaffeAmericano,
       coffeeName: "Blonde Caffè Americano",
-      coffeeDescription: "Espresso shots topped with hot water to produce a light layer of crema and are made with our Starbucks® Blonde Roast for an extra-smooth, subtly sweet and nuanced cup.. Pro Tip: For additional boost, ask your barista to try this with an extra shot (85 mg caffeine per shot)."
+      coffeeDescription: "Espresso shots topped with hot water to produce a light layer of crema and are made with our Starbucks® Blonde Roast for an extra-smooth, subtly sweet and nuanced cup.. Pro Tip: For additional boost, ask your barista to try this with an extra shot (85 mg caffeine per shot).",
+      stars: 2,
+      basePrice:5.54
     },
     {
-      coffeeIcon:caffeeMisto,
+      coffeeIcon: caffeeMisto,
       coffeeName: "Caffè Misto",
-      coffeeDescription: "A one-to-one combination of fresh-brewed coffee and steamed milk add up to one distinctly delicious coffee drink remarkably mixed."
+      coffeeDescription: "A one-to-one combination of fresh-brewed coffee and steamed milk add up to one distinctly delicious coffee drink remarkably mixed.",
+      stars: 4,
+      basePrice:5.91
+    },
+    {
+      coffeeIcon: darkRoastCoffee,
+      coffeeName: "Dark Roast Coffee",
+      coffeeDescription: "This full-bodied dark roast coffee with bold, robust flavors showcases our roasting and blending artistry—an essential blend of balanced and lingering flavors.",
+      stars: 3,
+      basePrice:3.27
+    },
+    {
+      coffeeIcon: cappuccino,
+      coffeeName: "Cappuccino",
+      coffeeDescription: "Dark, rich espresso lies in wait under a smoothed and stretched layer of thick milk foam. An alchemy of barista artistry and craft.",
+      stars: 5,
+      basePrice:4.66
+    },
+    {
+      coffeeIcon: espressoconPanna,
+      coffeeName: "Espresso con Panna",
+      coffeeDescription: "Espresso meets a dollop of whipped cream to enhance the rich and caramelly flavors of a straight-up shot.",
+      stars: 4,
+      basePrice:6.22
+    },
+    {
+      coffeeIcon: oatmilkHoneyLatte,
+      coffeeName: "Oatmilk Honey Latte",
+      coffeeDescription: "Oatmilk and signature espresso are intentionally combined with a hint of honey and a toasted honey topping—a savory tribute to all things sweet and salty.",
+      stars: 2,
+      basePrice:4.77
     },
   ]
 
   let [coffeeName, setCoffeeName] = useState(coffees[0].coffeeName);
-  let [description, setDescription] = useState(coffees[0].coffeeDescription);
+  let [coffeeDescription, setCoffeeDescription] = useState(coffees[0].coffeeDescription);
+  let [coffeeIcon, setCoffeeIcon] = useState(coffees[0].coffeeIcon);
+  let [coffeePrice, setCoffeePrice] = useState(coffees[0].basePrice);
+  let [coffeeStars, setCoffeeStars] = useState(coffees[0].stars);
 
   function changeCoffee(coffee) {
+    setCoffeeIcon(coffee.coffeeIcon);
     setCoffeeName(coffee.coffeeName);
-    setDescription(coffee.coffeeDescription);
+    setCoffeeDescription(coffee.coffeeDescription);
+    setCoffeePrice(coffee.basePrice);
+    setCoffeeStars(coffee.stars);
   }
 
   return (
     <section>
-      <CoffeeContentCell coffeeName={coffeeName} description={description} />
+      
+      <CoffeeContentCell coffeeName={coffeeName} description={coffeeDescription} coffeeicon={coffeeIcon} coffeeprice={coffeePrice} coffeestars={coffeeStars}/>
+
       <div className="coffee-list">
-        <CoffeeCell coffeeName={coffees[0].coffeeName} onclick={(e)=> changeCoffee(coffees[0])} />
-        <CoffeeCell coffeeName={coffees[1].coffeeName} onclick={(e)=> changeCoffee(coffees[1])} />
+
+        {coffees.map((value, index) => {
+          return <CoffeeCell coffeeName={value.coffeeName} onclick={(e) => changeCoffee(value)} />
+        })}
+
       </div>
     </section>
   );
