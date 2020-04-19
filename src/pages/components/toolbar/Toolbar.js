e8e812ecc1a3d0c1d37b9a86af079cbf864a3e5e
= <willyamvinicius1900@gmail.com>
2020-04-18 22:31:21 -0300

    added resposive

--- src/pages/components/toolbar/Toolbar.js
+++ src/pages/components/toolbar/Toolbar.js
@@ -3,11 +3,16 @@ import './css/toolbar.css';
 import logo from './images/logo.png';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Toolbar() {
 
   let [linkSelected, setSelectedLink] = useState(0);
+
+  let menuContainer = React.createRef();
+  let shadowMenuContainer = React.createRef();
+  let menuContainerElement;
+  let shadowMenuElement;
 
   let links = [
     {
@@ -38,22 +43,46 @@ export default function Toolbar() {
     }
   ];
 
+
+  function openAndCloseMenu() {
+    menuContainerElement = menuContainer.current;
+    shadowMenuElement = shadowMenuContainer.current;
+
+    if (menuContainerElement.offsetLeft !== 0) {
+      menuContainerElement.style.left = "0px";
+      shadowMenuElement.style.display = "block";
+    } else {
+      menuContainerElement.style.left = "-300px";
+      shadowMenuElement.style.display = "none";
+    }
+  }
+
   return (
     <header>
+      <div className="shadow-menu-container" ref={shadowMenuContainer} onClick={openAndCloseMenu}></div>
+      <div className="menu-container" ref={menuContainer}>
+        <img src={logo} alt="error"></img>
+
+        {links.map((value, index) => {
+          return <Link to={value.rote} onClick={() =>{setSelectedLink(value.index); openAndCloseMenu()}}>{value.linkText}</Link>;
+        })}
+
+      </div>
       <nav>
+        <FontAwesomeIcon icon={faBars} className="menu-button" onClick={openAndCloseMenu} />
         <img src={logo} alt="error"></img>
         <div className="toolbar-itens-container">
 
           {links.map((value, index) => {
 
             if (linkSelected === value.index) {
-              return <Link to={value.rote} className="selected-link" onClick={()=>setSelectedLink(value.index)}>{value.linkText}</Link>;
+              return <Link to={value.rote} className="selected-link" onClick={() => setSelectedLink(value.index)}>{value.linkText}</Link>;
             } else {
-              return <Link to={value.rote} onClick={()=>setSelectedLink(value.index)}>{value.linkText}</Link>
+              return <Link to={value.rote} onClick={() => setSelectedLink(value.index)}>{value.linkText}</Link>
             }
 
           })}
-          
+
         </div>
         <div className="background-search">
           <FontAwesomeIcon icon={faSearch} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,19 +5,25 @@ import image1 from './images/image1.png';
 import image2 from './images/image2.png';
 
 export default function Home() {
+
+
+  function openLink(url) {
+    window.open(url, "_blank")
+  }
+
   return (
     <div className="content">
       <div className="image-home-container">
         <img src={image1} alt=""></img>
         <h3>$0 delivery fee for Starbucks orders on Uber Eats</h3>
         <p>Get your favorite food and drinks delivered.</p>
-        <button>Order now</button>
+        <button onClick={()=> openLink("https://1qt-1.tlnk.io/serve?action=click&site_id=113033&url_web=https://www.ubereats.com/brand/starbucks?&utm_campaign=ext-starbucks-sbxapp&$3p=a_custom_361342&sub_publisher=Uber%20Eats%20-%20BD%20(Biz%20Dev)%20-%20Partnerships&invoke_url=ubereats://chain/browse?chainName=starbucks&sub_campaign=ext-starbucks-sbxapp")}>Order now</button>
       </div>
       <div className="image-home-container">
         <img src={image2} alt=""></img>
         <h3>Brew and enjoy from the comfort of home</h3>
         <p>Get your Starbucks favorites from your grocery provider.</p>
-        <button>Learn more</button>
+        <button onClick={()=> openLink("https://athome.starbucks.com/")}>Learn more</button>
       </div>
     </div>
   );
